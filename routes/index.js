e6d3925be864dd53f9422ad39da639c793bbbd20
Nicholette <nekonekonik@gmail.com>
2016-01-10 17:30:18 -0800

    more setting up

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var logger = new Console(output, errorOutput);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
